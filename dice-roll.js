e5eaf2b0950843b1c54330940955fa83413f4777
LinnetDev <m.gorbachova13@gmail.com>
2025-06-21 19:59:58 -0400

    Add files via upload

---
+++ dice-roll.js
@@ -0,0 +1,17 @@
+function rollDice() {
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+    document.querySelector(".img1").src = `images/dice${randomNumber1}.png`;
+    document.querySelector(".img2").src = `images/dice${randomNumber2}.png`;
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").textContent = "🚩 Player 1 WINS";
+    } else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").textContent = "Player 2 WINS 🚩";
+    } else {
+        document.querySelector("h1").textContent = "DRAW";
+    }
+}
+
+rollDice();
